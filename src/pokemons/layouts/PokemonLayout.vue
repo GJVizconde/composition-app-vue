a53305642bb40d32ada340702c43adc15f5b2c9b
Gilmer J. Vizconde <gilmerjose.vizconde2@gmail.com>
2024-02-22 10:58:52 -0500

    first commit

---
+++ src/pokemons/layouts/PokemonLayout.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import NavBar from '@/shared/components/NavBar.vue';
+import type { RounterLink } from '@/router/list-routes';
+
+import { pokemonRoute } from '../router/index';
+
+const routeLinks:RounterLink[] = pokemonRoute.children?.map( ({name, path, props}) => {
+    
+
+    return {
+        name: name?.toString() ?? '',
+        path: path,
+        title: (props as { title:string}).title
+    }
+
+} ) || []
+
+
+</script>
+<template>
+    <NavBar 
+    :links="routeLinks"
+    isSecondary
+    />
+    <div>
+        <!-- <h4>Pokemon Layout</h4> -->
+    
+
+    <RouterView />
+</div>
+</template>
---
+++ src/pokemons/pages/PokemonById.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <div>
+        <h1>Pokemon Id</h1>
+    </div>
+</template>
